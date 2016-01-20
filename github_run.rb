449d8e88537701f3dec3e480c1f4e4b6aa1b4501
Deepa Kackar <dkackar99@yahoo.com>
2016-01-19 18:01:24 -0800

    Added github_forkcommit

--- github_run.rb
+++ github_run.rb
@@ -2,13 +2,12 @@ require_relative 'github_https.rb'
 require 'pp'
 
  response = GithubHttps.new
-# raw_repo_list = response.list_repos
-# raw_commit_history = response.list_commits
+raw_repo_list = response.list_repos
+raw_commit_history = response.list_commits
 
-# puts "Last 10 Repositories:\n"
-# pp response.parse_repos(raw_repo_list)
+puts "Last 10 Repositories:\n"
+pp response.parse_repos(raw_repo_list)
 
-# puts "Up to last 10 commits for 10 recent repos:\n"
-# pp response.parse_commits(raw_commit_history)
+puts "Up to last 10 commits for 10 recent repos:\n"
+pp response.parse_commits(raw_commit_history)
 
-response.create_repo
